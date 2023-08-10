41606bb6c59805e283429abfd7a52d6ba690b2a7
Summer T <summer@summertime-development.com>
2023-08-10 12:03:03 -0500

    added a build command

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ChCo = void 0;
 //@ts-ignore
-import tinygradient from 'tinygradient';
-export class ChCo {
-    constructor(keyword) {
-        this.consoleRed = (value) => {
+var tinygradient_1 = require("tinygradient");
+var ChCo = exports.ChCo = /** @class */ (function () {
+    function ChCo() {
+        this.consoleRed = function (value) {
             console.log('\x1b[0;31m', value);
         };
-        this.consoleOrange = (value) => {
+        this.consoleOrange = function (value) {
             console.log('\x1b[38;2;255;100;0m', value);
         };
-        this.consoleYellow = (value) => {
+        this.consoleYellow = function (value) {
             console.log('\x1b[0;33m', value);
         };
-        this.consoleGreen = (value) => {
+        this.consoleGreen = function (value) {
             console.log('\x1b[0;32m', value);
         };
-        this.consoleBlue = (value) => {
+        this.consoleBlue = function (value) {
             console.log('\x1b[0;36m', value);
         };
-        this.consolePurple = (value) => {
+        this.consolePurple = function (value) {
             console.log('\x1b[38;2;179;124;255m', value);
         };
-        this.consoleWhite = (value) => {
+        this.consoleWhite = function (value) {
             console.log('\x1b[0m', value);
         };
-        this.consoleRedOrGreen = (value) => {
-            let str = value.replace('!', '').replace('!', '') + '';
+        this.consoleRedOrGreen = function (value) {
+            var str = value.replace('!', '').replace('!', '') + '';
             if (eval(value)) {
                 console.log('\x1b[0;32m', str + ' => ' + eval(value));
             }
@@ -32,128 +35,145 @@ export class ChCo {
                 console.log('\x1b[0;31m', str + ' => ' + eval(value));
             }
         };
-        if (!!keyword) {
-            ChCo.setColor(keyword);
-            ChCo.gradientStringBackUp = keyword;
-        }
-        else {
-            ChCo.gradient = tinygradient(ChCo.gradientShorthands.softrainbow);
-            ChCo.gradientStringBackUp = 'softrainbow';
-        }
     }
-    static getGradient(keyword) {
-        const gradient = Object.fromEntries(Object.entries(ChCo.gradientShorthands).filter(([key]) => key.includes(keyword)));
-        const holder = Object.values(gradient);
-        return tinygradient(holder[0]);
-    }
-    //todo - make this better
-    static setColor(keyword) {
+    ChCo.getGradient = function (keyword) {
+        if (!keyword) {
+            return (0, tinygradient_1.default)(this.gradientShorthands.softrainbow);
+        }
+        var gradient = Object.fromEntries(Object.entries(ChCo.gradientShorthands).filter(function (_a) {
+            var key = _a[0];
+            return key.includes(keyword);
+        }));
+        var holder = Object.values(gradient);
+        return (0, tinygradient_1.default)(holder[0]);
+    };
+    ChCo.setColor = function (keyword) {
         if (keyword) {
             ChCo.gradient = this.getGradient(keyword);
         }
         else {
-            ChCo.gradient = tinygradient(ChCo.gradientShorthands.softrainbow);
+            ChCo.gradient = (0, tinygradient_1.default)(ChCo.gradientShorthands.softrainbow);
         }
-    }
-    static log(inputString) {
+    };
+    ChCo.log = function (inputString) {
         console.log('\x1b[0m', this.formatString(inputString));
-    }
-    buffer() {
-        const holder = '■▣'.repeat(50);
+    };
+    ChCo.prototype.buffer = function () {
+        var holder = '■▣'.repeat(50);
         console.log('\x1b[0m', ChCo.formatString(holder));
-    }
-    static formatString(input) {
-        const backupGraident = ChCo.gradient;
-        if (input.length < ChCo.gradient.stops.length) {
-            // TODO - redo this logic, it sets the gradient permanently right now
-            // ChromaConsole.gradient.stops = [
-            //     ChromaConsole.gradient.stops[0],
-            //     ChromaConsole.gradient.stops[1],
-            // ];
-            console.log(this.gradientStringBackUp);
-            //console.log(ChCo.gradient.stops.length);
-            //var holder = ChCo.gradient.stops.slice(1);
-            let holder2 = this.getGradient(this.gradientStringBackUp);
-            //var holder = ChCo.gradient.stops.slice(1);
-            console.log(holder2);
+    };
+    ChCo.formatString = function (input) {
+        var _a, _c;
+        var backupGraident = ChCo.gradient;
+        if (input.length == 0) {
             return input;
         }
-        var colorArray = ChCo.gradient.rgb(input.length);
-        let output = '';
-        for (let i = 0; i < input.length; i++) {
-            var { _r, _g, _b } = colorArray[i];
-            output += `\x1b[38;2;${Math.round(_r)};${Math.round(_g)};${Math.round(_b)}m${input[i]}`;
+        else if (input.length < 3) {
+            var editedGradient = ChCo.gradient.stops.slice(0, input.length);
+            var output = '';
+            for (var i = 0; i < input.length; i++) {
+                var _r = (_a = editedGradient[i].color, _a._r), _g = _a._g, _b = _a._b;
+                output += "\u001B[38;2;".concat(Math.round(_r), ";").concat(Math.round(_g), ";").concat(Math.round(_b), "m").concat(input[i]);
+            }
+            output += '\x1b[0m';
+            return output;
         }
-        output += '\x1b[0m';
-        ChCo.gradient = backupGraident;
-        return output;
-    }
-}
-ChCo.start = () => {
-    console.log('\x1b[0m', ChCo.formatString('■▣'.repeat(50)));
-    console.log('\x1b[0m', ChCo.formatString('■▣'.repeat(21) + ' Starting up ' + '■▣'.repeat(22)));
-    console.log('\x1b[0m', ChCo.formatString('■▣'.repeat(50)));
-    console.log('');
-};
-ChCo.end = () => {
-    console.log('');
-    console.log('\x1b[0m', ChCo.formatString('■▣'.repeat(50)));
-    console.log('\x1b[0m', ChCo.formatString('■▣'.repeat(21) + ' End of program ' + '■▣'.repeat(22)));
-    console.log('\x1b[0m', ChCo.formatString('■▣'.repeat(50)));
-};
-ChCo.debug = () => {
-    ChCo.start();
-    ChCo.end();
-};
-ChCo.gradientShorthands = {
-    vaporwave: ['#0000ff', '#ff3399', '#00ffcc'],
-    softrainbow: [
-        '#c1153d',
-        '#dd901c',
-        '#efe52d',
-        '#5eef2d',
-        '#2750f4',
-        '#2914e5',
-    ],
-    oldmovie: [
-        '#F8F9FA',
-        '#E9ECEF',
-        '#DEE2E6',
-        '#CED4DA',
-        '#ADB5BD',
-        '#6C757D',
-        '#495057',
-        '#343A40',
-        '#212529',
-    ],
-    firewood: [
-        '#03071E',
-        '#370617',
-        '#6A040F',
-        '#9D0208',
-        '#D00000',
-        '#DC2F02',
-        '#E85D04',
-        '#F48C06',
-        '#FAA307',
-    ],
-};
+        else {
+            if (input.length < ChCo.gradient.stops.length) {
+                var holder2 = ChCo.gradient.stops.slice(0, input.length - 1);
+                ChCo.gradient = (0, tinygradient_1.default)(holder2);
+            }
+            var colorArray = ChCo.gradient.rgb(input.length);
+            var output = '';
+            for (var i = 0; i < input.length; i++) {
+                var _r = (_c = colorArray[i], _c._r), _g = _c._g, _b = _c._b;
+                output += "\u001B[38;2;".concat(Math.round(_r), ";").concat(Math.round(_g), ";").concat(Math.round(_b), "m").concat(input[i]);
+            }
+            output += '\x1b[0m';
+            ChCo.gradient = backupGraident;
+            return output;
+        }
+    };
+    ChCo.gradientShorthands = {
+        purplehaze: ['#9900ff', '#cc99ff'],
+        vaporwave: ['#0000ff', '#ff3399', '#00ffcc'],
+        softrainbow: [
+            '#c1153d',
+            '#dd901c',
+            '#efe52d',
+            '#5eef2d',
+            '#2750f4',
+            '#2914e5',
+        ],
+        oldmovie: [
+            '#F8F9FA',
+            '#E9ECEF',
+            '#DEE2E6',
+            '#CED4DA',
+            '#ADB5BD',
+            '#6C757D',
+            '#495057',
+            '#343A40',
+            '#212529',
+        ],
+        firewood: [
+            '#03071E',
+            '#370617',
+            '#6A040F',
+            '#9D0208',
+            '#D00000',
+            '#DC2F02',
+            '#E85D04',
+            '#F48C06',
+            '#FAA307',
+        ],
+    };
+    ChCo.gradient = (0, tinygradient_1.default)(ChCo.gradientShorthands.softrainbow);
+    ChCo.loren = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    ChCo.start = function () {
+        console.log('\x1b[0m', ChCo.formatString('■▣'.repeat(50)));
+        console.log('\x1b[0m', ChCo.formatString('■▣'.repeat(21) + ' Starting up ' + '■▣'.repeat(22)));
+        console.log('\x1b[0m', ChCo.formatString('■▣'.repeat(50)));
+        console.log('');
+    };
+    ChCo.end = function () {
+        console.log('');
+        console.log('\x1b[0m', ChCo.formatString('■▣'.repeat(50)));
+        console.log('\x1b[0m', ChCo.formatString('■▣'.repeat(21) + ' End of program ' + '■▣'.repeat(22)));
+        console.log('\x1b[0m', ChCo.formatString('■▣'.repeat(50)));
+    };
+    ChCo.debug = function () {
+        ChCo.start();
+        ChCo.log(ChCo.loren);
+        ChCo.end();
+    };
+    ChCo.characterDebug = function () {
+        ChCo.log('a');
+        ChCo.log('aa');
+        ChCo.log('aaa');
+        ChCo.log('aaaa');
+        ChCo.log('aaaaa');
+        ChCo.log('aaaaaaaaaa');
+    };
+    ChCo.superDebug = function () {
+        ChCo.characterDebug();
+        ChCo.debug();
+        ChCo.setColor('purplehaze');
+        ChCo.characterDebug();
+        ChCo.debug();
+        ChCo.setColor('vaporwave');
+        ChCo.characterDebug();
+        ChCo.debug();
+        ChCo.setColor('oldmovie');
+        ChCo.characterDebug();
+        ChCo.debug();
+        ChCo.setColor('firewood');
+        ChCo.characterDebug();
+        ChCo.debug();
+    };
+    return ChCo;
+}());
 function test() {
-    ChCo.debug();
-    ChCo.setColor('oldmovie');
-    ChCo.debug();
-    ChCo.setColor('vaporwave');
-    ChCo.debug();
-    ChCo.setColor('firewood');
-    ChCo.debug();
-    ChCo.setColor('rainbow');
-    ChCo.debug();
+    ChCo.superDebug();
 }
 //test();
-function test2() {
-    ChCo.setColor('firewood');
-    ChCo.log('aaa');
-    //ChCo.log('a');
-    //ChCo.log('aaaaaaaaaaaa');
-}
-test2();
